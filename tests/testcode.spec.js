cdb897730c19436ee35eac3c316bce8197d511ca
khaleel <kaleel15593@gmail.com>
2024-10-11 17:53:31 +0530

    switching between windows

---
+++ tests/testcode.spec.js
@@ -0,0 +1,85 @@
+const {test, expect} = require('@playwright/test');
+
+
+test('google page open', async({browser})=>
+    
+
+    {
+//initiate fresh browser and browser
+    const context= await browser.newContext();
+    const page= await context.newPage();
+   await  page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+   //capture the title of the page and match with the expected title
+   console.log(await page.title());
+   await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
+   //enter incorrect login credentials and capture the error message
+   await page.locator('#username').fill("rahushttyacademy");
+   await page.locator('#password').fill("learning");
+   await page.locator('#signInBtn').click();
+   console.log(await page.locator("[style*='block']").textContent());
+   await expect(page.locator("[style*='block']")).toContainText("password");
+   //replace with valid credentials and login
+   await page.locator('#username').fill("rahulshettyacademy");
+   await page.locator('#password').fill("learning");
+   await page.locator('#signInBtn').click();
+   //click on shop tab 
+   await page.locator("[href='/angularpractice/shop']").click();
+   //capture text of first and all shopping items from the list
+   console.log (await page.locator('.card-body a').first().textContent());
+   console.log (await page.locator('.card-body a').nth(0).textContent());
+   console.log (await page.locator('.card-body a').allTextContents());
+
+});
+
+test('multiple field type  scenario', async({page})=>
+{
+    await  page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+   //capture the title of the page and match with the expected title
+   console.log(await page.title());
+   await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
+   //enter incorrect login credentials and capture the error message
+   await page.locator('#username').fill("rahulshettyacademy");
+   await page.locator('#password').fill("learning");
+
+   //selecting dropdown fields
+   const dropdown= page.locator("select.form-control");
+  await dropdown.selectOption("consult");
+  //click on radio button
+  await page.locator(".radiotextsty").last().click();
+   // handling popup window
+   await page.locator("#okayBtn").click();
+
+  console.log(await page.locator(".radiotextsty").last().isChecked());
+await page.locator('#terms').click();
+//click on check box
+ await page.locator("#signInBtn").click();
+ //unchecking check box 
+ await page.locator('#terms').uncheck();
+ expect(await page.locator('#terms').isChecked()).toBeFalsy();
+
+
+await page.pause();
+
+
+});
+    
+test.only('switching to child window', async({browser})=>
+    {
+        const context= await browser.newContext();
+    const page= await context.newPage();
+        await  page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+        const linktext= page.locator("[href*='documents-req']");
+
+
+        const [newpage]=await Promise.all([
+
+context.waitForEvent('page'),
+await linktext.click(),
+        ])
+
+       const labeltext = await newpage.locator(".red").textContent();
+        console.log(labeltext);
+        page.pause();
+
+
+    });
